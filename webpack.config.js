2f0021990b404df3f8e58ebd8b2242b99d0ad2d0
MTLightingDev <16318614+MTLightingDev@users.noreply.github.com>
2026-02-12 11:10:11 -0700

    update webpack config for broader JSX support and upgrade dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -59,12 +59,11 @@ module.exports = {
         }],
       },
       {
-        test: /.jsx?$/,
+        test: /\.m?jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!(@shopify\/draggable)\/).*/,
         query: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
-          // presets: ['env', 'react']
         },
       },
     ],
